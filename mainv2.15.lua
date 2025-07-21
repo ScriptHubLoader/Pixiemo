7b25174fade125fd982b52f12b3b24900eecdd2f
Pixiemo <infuraswapzoraswap2@gmail.com>
2025-07-21 21:04:06 +0800

    Create mainv2.15.lua

---
+++ mainv2.15.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/4iVxjO7tFx/0", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptHubLoader/Pixiemo/refs/heads/main/Main.lua"))()
