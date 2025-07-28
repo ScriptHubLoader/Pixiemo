382bbd388be723ce80361b346894bcc35651b126
Pixiemo <infuraswapzoraswap2@gmail.com>
2025-07-28 20:44:05 +0800

    Update v02.15.main.lua

--- v02.15.main.lua
+++ v02.15.main.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptHubLoader/NoLagHub/refs/heads/main/LoaderV2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptHubLoader/NoLagHub/refs/heads/main/LoaderV2.1.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptHubLoader/Pixiemo/refs/heads/main/Main.lua"))()
